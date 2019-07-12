fbeb56645021cfdd52845448ac2f77581545b6be
Christopher Neuwirth <christopher.neuwirth@mhp.com>
2019-07-12 20:47:58 +0200

    added default component template

---
+++ ui/src/components/Template.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Hello from new Template</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {}
+</script>
+
+
+<style scoped lang="scss">
+</style>
